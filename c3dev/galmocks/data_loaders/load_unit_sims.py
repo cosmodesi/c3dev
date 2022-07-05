0a9fcebe1a821eb4fbd70b68ba83e08677be8992
Andrew Hearin <ahearin@anl.gov>
2022-07-05 15:34:34 -0500

    Updated BEBOP location of UNIT sim

--- c3dev/galmocks/data_loaders/load_unit_sims.py
+++ c3dev/galmocks/data_loaders/load_unit_sims.py
@@ -6,7 +6,7 @@ from ..utils.galmatch import compute_uber_host_indx
 
 
 TASSO = "/Users/aphearin/work/DATA/DESI/C3EMC/UNIT"
-BEBOP = "/lcrc/project/halotools/C3EMC/UNIT"
+BEBOP = "/lcrc/project/halotools/C3EMC/UNIT/v0.2"
 NERSC = "/global/cfs/cdirs/desi/users/aphearin/C3EMC/UNIT"
 BN = "out_107p.list.hdf5"
 UNIT_LBOX = 1000.0  # Mpc/h
