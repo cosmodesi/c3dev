b1d0ddb432f0ad88c2feadbcbd36764c9ee559bd
Andrew Hearin <ahearin@anl.gov>
2022-09-10 13:59:34 -0500

    Changed default version of gumbo to v0.3

--- c3dev/galmocks/data_loaders/load_gumbo.py
+++ c3dev/galmocks/data_loaders/load_gumbo.py
@@ -5,7 +5,7 @@ from astropy.table import Table
 
 
 NERSC_DRN = "/global/cfs/cdirs/desi/users/aphearin/C3EMC/gumbo"
-LATEST = "v0.2"
+LATEST = "v0.3"
 
 
 def read_gumbo_mock(fn=None, drn=NERSC_DRN, version=LATEST):
@@ -34,6 +34,8 @@ def _get_gumbo_basename(version):
         bn = "gumbo_v0.1.h5"
     elif version == "v0.2":
         bn = "gumbo_v0.2.h5"
+    elif version == "v0.3":
+        bn = "gumbo_v0.3.h5"
     else:
-        raise ValueError("No other available versions of the gumbo mock")
+        raise ValueError("gumbo mock version `{}` is unavailable".format(version))
     return bn
