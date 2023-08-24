40d051a4768b4b43452642f8a8442db743a00049
gbeltzmo <gillian.d.beltz-mohrmann@vanderbilt.edu>
2023-08-24 16:49:48 -0500

    rename galsample script

--- c3dev/galmocks/scripts/galsample_script.sh
+++ c3dev/galmocks/scripts/galsample_script.sh
@@ -11,6 +11,6 @@ source ~/anaconda3/etc/profile.d/conda.sh
 conda activate diffwhatever
 
 # Run python script
-srun python make_mock_UNIT_UM.py fixedAmp_001_hlist_0.67120.list.hdf5 UM_SMDPL_a0p6643_mock.hdf5 mock_z0p5 0.5053 0.4899 fixedAmp_001_galsampled_mock_67120.hdf5
+srun python make_diffsky_mock.py fixedAmp_001_hlist_0.67120.list.hdf5 UM_SMDPL_a0p6643_mock.hdf5 mock_z0p5 0.5053 0.4899 fixedAmp_001_galsampled_mock_67120.hdf5
 
 conda deactivate
